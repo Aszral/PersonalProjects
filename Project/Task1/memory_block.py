e9915326c1a0b2bce6c8482ca840fdb6f3752bc2
Luis Gonzalez <lg222zm@student.lnu.se>
2022-12-24 15:42:46 +0100

    m

--- Project/Task1/memory_block.py
+++
@@ -1,9 +0,0 @@
-from io_functions import *
-
-# Represents memory block in the heap.
-class MemoryBlock:
-    def __init__(self, start_address, size, allocated=False):
-        self.start_address = start_address
-        self.end_address = start_address + size
-        self.size = size
-        self.allocated = allocated
