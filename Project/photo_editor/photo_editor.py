f845328125e27dfa991a378176cac2adf5170323
Luis Gonzalez <lg222zm@student.lnu.se>
2022-12-25 16:00:13 +0100

    paths

--- Project/photo_editor/photo_editor.py
+++ Project/photo_editor/photo_editor.py
@@ -1,11 +1,12 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = "./edited_photos"
-path_Out = "./unedited_photos"
+path = "./unedited_photos"
+path_Out = "./edited_photos"
 
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
     edit = img.filter(ImageFilter.SHARPEN)
     clean_name = os.path.splitext(filename)[0]
     edit.save(f".{path_Out}/{clean_name}_edited.jpg")
+
